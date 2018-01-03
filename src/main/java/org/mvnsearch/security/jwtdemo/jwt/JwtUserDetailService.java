65c7444ee6ac9ecc1b5eb63ecb52b7b343e72722
linux_china <linux_china@hotmail.com>
2018-01-03 11:06:51 +0800

    user detail service

---
+++ src/main/java/org/mvnsearch/security/jwtdemo/jwt/JwtUserDetailService.java
@@ -0,0 +1,21 @@
+package org.mvnsearch.security.jwtdemo.jwt;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Arrays;
+
+/**
+ * user detail service
+ *
+ * @author linux_china
+ */
+public class JwtUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User(username, "", Arrays.asList(new SimpleGrantedAuthority("USER"), new SimpleGrantedAuthority("ROLE_USER")));
+    }
+}
