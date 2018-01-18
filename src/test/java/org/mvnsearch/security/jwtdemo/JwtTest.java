7adf12687182b976e9f0edd19a2932656b5c8767
linux_china <linux_china@hotmail.com>
2018-01-18 15:12:50 +0800

    add cache support

--- src/test/java/org/mvnsearch/security/jwtdemo/JwtTest.java
+++ src/test/java/org/mvnsearch/security/jwtdemo/JwtTest.java
@@ -1,27 +1,63 @@
 package org.mvnsearch.security.jwtdemo;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
- * JWT test
+ * JWT test with cache support
  *
  * @author linux_china
  */
 public class JwtTest {
 
+    private static JWTVerifier jwtVerifier;
+    private static Algorithm algorithmHS;
+    private static LoadingCache<String, DecodedJWT> tokenStore;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        algorithmHS = Algorithm.HMAC256("secret");
+        jwtVerifier = JWT.require(algorithmHS).withIssuer("mvnsearch").build();
+        tokenStore = Caffeine.newBuilder()
+                .maximumSize(10_000)
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .refreshAfterWrite(1, TimeUnit.MINUTES)
+                .build(token -> jwtVerifier.verify(token));
+    }
+
+
     @Test
     public void testGenerateToken() throws Exception {
-        Algorithm algorithmHS = Algorithm.HMAC256("secret");
         String token = JWT.create()
                 .withIssuer("mvnsearch")
-                .withAudience("Admin")
-                .withSubject("jackie")
+                .withSubject("user1")
                 .withIssuedAt(new Date())
+                .withArrayClaim("authorities", new String[]{"SMS"})
                 .sign(algorithmHS);
         System.out.println(token);
     }
+
+    @Test
+    public void testVerify() throws Exception {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyMSIsImlzcyI6Im12bnNlYXJjaCIsImlhdCI6MTUxNjI1OTQ3MiwiYXV0aG9yaXRpZXMiOlsiU01TIl19.5-tYkDPzNItMG3Sq7iXjZHOIisCAflIP6IGKNreTo7A";
+        DecodedJWT jwt = tokenStore.get(token);
+        Claim claim = jwt.getClaim("authorities");
+        String[] authorities = claim.asArray(String.class);
+        Assert.assertNotNull(authorities);
+        for (String authority : authorities) {
+            System.out.println(authority);
+        }
+    }
+
 }
