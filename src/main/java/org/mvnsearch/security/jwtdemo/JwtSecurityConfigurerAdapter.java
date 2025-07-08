85a8915c085b60128a5b398708bf562dee3e9ae1
Libing Chen <libing.chen@gmail.com>
2025-07-08 23:26:17 +0800

    chore: upgrade

--- src/main/java/org/mvnsearch/security/jwtdemo/JwtSecurityConfigurerAdapter.java
+++ src/main/java/org/mvnsearch/security/jwtdemo/JwtSecurityConfigurerAdapter.java
@@ -3,19 +3,19 @@ package org.mvnsearch.security.jwtdemo;
 import org.mvnsearch.security.jwtdemo.jwt.JWTAuthenticationFilter;
 import org.mvnsearch.security.jwtdemo.jwt.JwtAuthenticationProvider;
 import org.mvnsearch.security.jwtdemo.jwt.JwtUserDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 /**
  * jwt security configurer adapter
@@ -24,50 +24,44 @@ import org.springframework.security.web.context.SecurityContextPersistenceFilter
  */
 @Configuration
 @EnableWebSecurity
-public class JwtSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-    /**
-     * white urls for static resources
-     */
-    @Value("${security.ignored}")
-    public String[] whiteUrls;
+public class JwtSecurityConfigurerAdapter extends WebSecurityConfiguration {
+  /**
+   * white urls for static resources
+   */
+  @Value("${security.ignored}")
+  public String[] whiteUrls;
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterAfter(new JWTAuthenticationFilter(), SecurityContextPersistenceFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers(whiteUrls).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .httpBasic().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> {
-                    response.sendError(401, "Access Denied: please add legal JWT token on Authorization(HTTP header). Detail: " + authException.getMessage() + " If you have problem, please contact linux_china");
-                })
-                .accessDeniedHandler((request, response, accessDeniedException) -> {
-                    response.sendError(403, "Access Denied: please check your authorities. If you have problem, please contact linux_china");
-                });
-    }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.addFilterAfter(new JWTAuthenticationFilter(), SecurityContextHolderFilter.class)
+      .securityMatcher(whiteUrls).authorizeHttpRequests(registry -> registry.anyRequest().permitAll())
+      .authorizeHttpRequests((authz) -> authz
+        .anyRequest().authenticated()
+      )
+      .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .csrf(AbstractHttpConfigurer::disable)
+      .exceptionHandling(configurer -> {
+        configurer.authenticationEntryPoint((request, response, authException) -> {
+          response.sendError(401, "Access Denied: please add legal JWT token on Authorization(HTTP header). Detail: " + authException.getMessage() + " If you have problem, please contact linux_china");
+        });
+        configurer.accessDeniedHandler((request, response, accessDeniedException) -> {
+        });
+      }).build();
+  }
 
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (webSecurity) -> webSecurity.ignoring().requestMatchers(whiteUrls);
+  }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth, AuthenticationProvider authenticationProvider, UserDetailsService userDetailsService) throws Exception {
-        auth.authenticationProvider(authenticationProvider).userDetailsService(userDetailsService);
-    }
+  @Bean
+  public AuthenticationProvider jwtAuthenticationProvider(UserDetailsService userDetailsService) throws Exception {
+    return new JwtAuthenticationProvider(userDetailsService);
+  }
 
-    @Bean
-    public AuthenticationProvider jwtAuthenticationProvider(UserDetailsService userDetailsService) throws Exception {
-        return new JwtAuthenticationProvider(userDetailsService);
-    }
+  @Bean
+  public UserDetailsService jwtUserDetailsService() {
+    return new JwtUserDetailService();
+  }
 
-    @Bean
-    public UserDetailsService jwtUserDetailsService() {
-        return new JwtUserDetailService();
-    }
-
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 }
